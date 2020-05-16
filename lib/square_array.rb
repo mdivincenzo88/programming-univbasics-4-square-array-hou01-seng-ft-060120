c3129e44a3ecdae2e0fb48be786957b8ff0a7986
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-05-16 03:25:38 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,10 @@
 def square_array(array)
   count = 0
   new_array = []
-  while array.length < count do
-    array [count] = array [count] * array[count]
+  while array.length <= count do
+    new_array [count] = array [count] * array[count]
     count = count + 1
-    new_array [count] == array [count]
+    
 end
 new_array
 end
